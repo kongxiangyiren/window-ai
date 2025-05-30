cdb02d1716edf19f4798fc18fa43367d07aa2592
空巷一人 <1530688385@qq.com>
2025-05-30 10:25:42 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from './App.vue'
 import '@devui-design/icons/icomoon/devui-icon.css'
 
 import Icon from 'vue-devui/icon'
-// import 'vue-devui/icon/style.css'
+import 'vue-devui/icon/style.css'
 
 const app = createApp(App)
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,90 +3,77 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
+// import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    importToCDN({
-      prodUrl: '//unpkg.com/{name}@{version}/{path}',
-      // enableInDevMode: true,
-      modules: [
-        'vue',
-        {
-          name: 'markdown-it',
-          var: 'markdownit',
-          path: 'dist/markdown-it.min.js',
-        },
-        {
-          name: 'highlight.js',
-          var: 'hljs',
-          path: '//lib.baomitu.com/highlight.js/11.7.0/highlight.min.js',
-        },
-        {
-          name: 'vue-devui',
-          var: 'VueDevui',
-          path: `vue-devui.umd.js`,
-          css: ['style.css'],
-        },
-      ],
-    }),
-    VitePWA({
-      devOptions: {
-        enabled: false,
-      },
-      registerType: 'autoUpdate',
-      injectRegister: 'inline',
-      manifest: {
-        description: 'Window AI',
-        theme_color: '#DDE3E9',
-        background_color: '#DDE3E9',
-        icons: [
-          //添加图标， 注意路径和图像像素正确
-          {
-            src: '512x.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      // workbox: {
-      //   runtimeCaching: [
-      //     // woff?1111 结尾要缓存
-      //     {
-      //       urlPattern: /.*woff\?\d+$/,
-      //       handler: 'CacheFirst',
-      //       options: {
-      //         cacheName: 'font-cache',
-      //         expiration: {
-      //           maxEntries: 100,
-      //           maxAgeSeconds: 60 * 60 * 24 * 365,
-      //         },
-      //       },
-      //     },
-      //   ],
-      // },
-    }),
+    // importToCDN({
+    //   prodUrl: '//unpkg.com/{name}@{version}/{path}',
+    //   // enableInDevMode: true,
+    //   modules: [
+    //     'vue',
+    //     {
+    //       name: 'markdown-it',
+    //       var: 'markdownit',
+    //       path: 'dist/markdown-it.min.js',
+    //     },
+    //     {
+    //       name: 'highlight.js',
+    //       var: 'hljs',
+    //       path: '//lib.baomitu.com/highlight.js/11.7.0/highlight.min.js',
+    //     },
+    //     {
+    //       name: 'vue-devui',
+    //       var: 'VueDevui',
+    //       path: `vue-devui.umd.js`,
+    //       css: ['style.css'],
+    //     },
+    //   ],
+    // }),
+    // VitePWA({
+    //   devOptions: {
+    //     enabled: false,
+    //   },
+    //   registerType: 'autoUpdate',
+    //   injectRegister: 'inline',
+    //   manifest: {
+    //     description: 'Window AI',
+    //     theme_color: '#DDE3E9',
+    //     background_color: '#DDE3E9',
+    //     icons: [
+    //       //添加图标， 注意路径和图像像素正确
+    //       {
+    //         src: '512x.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //       },
+    //     ],
+    //   },
+    //   // workbox: {
+    //   //   runtimeCaching: [
+    //   //     // woff?1111 结尾要缓存
+    //   //     {
+    //   //       urlPattern: /.*woff\?\d+$/,
+    //   //       handler: 'CacheFirst',
+    //   //       options: {
+    //   //         cacheName: 'font-cache',
+    //   //         expiration: {
+    //   //           maxEntries: 100,
+    //   //           maxAgeSeconds: 60 * 60 * 24 * 365,
+    //   //         },
+    //   //       },
+    //   //     },
+    //   //   ],
+    //   // },
+    // }),
   ],
   base: '/window-ai/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            console.log(id)
-
-            return id.toString().split('node_modules/')[1].split('/')[0].toString()
-          }
-        },
-      },
     },
   },
 })
